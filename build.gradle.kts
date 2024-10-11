62b0bb16103109f91852fbfcf163ae686b9dc52f
Ha Van Binh <havanbinh22022003@gmail.com>
2024-10-11 14:26:17 +0700

    feat(hellowordl): #1 finish task hello world

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,12 +10,13 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
     // https://mvnrepository.com/artifact/org.slf4j/slf4j-api
     implementation("org.slf4j:slf4j-api:2.0.16")
     // https://mvnrepository.com/artifact/org.slf4j/slf4j-log4j12
     implementation("org.slf4j:slf4j-log4j12:2.0.16")
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+
 
 }
 
