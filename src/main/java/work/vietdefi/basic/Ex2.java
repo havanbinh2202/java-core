d6401f133d5a7d84a0b11db1471e6889cc8c9c11
Ha Van Binh <havanbinh22022003@gmail.com>
2024-10-11 17:31:21 +0700

    feat(java-basic): #3 Ex2 update

--- src/main/java/work/vietdefi/basic/Ex2.java
+++ src/main/java/work/vietdefi/basic/Ex2.java
@@ -3,14 +3,15 @@ package work.vietdefi.basic;
 public class Ex2 {
 
     /**
-     * Tính tổng các số từ 1 đến giá trị đầu vào.
+     * Determines if the given number is even.
      *
-     * @param input số lớn nhất để tính tổng.
-     * @return tổng từ 1 đến input.
+     * @param input the number to check
+     * @return true if the input is even; false if the input is odd
      */
+
     public int sum(int input) {
         if (input <= 0){
-            return 0;    // Trả về 0 nếu đầu vào là 0 hoặc âm
+            return 0;    // returns 0 if input is 0
         }
         int sum = 0;
         for (int i = 1; i <= input; i++) {
