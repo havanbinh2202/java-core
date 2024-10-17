9e8b87c43e49358c136378caabf0caea9bd972b8
Ha Van Binh <havanbinh22022003@gmail.com>
2024-10-17 09:28:48 +0700

    feat(java-sql): #10 Connect Java and SQL database

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,10 @@ dependencies {
     implementation("com.google.code.gson:gson:2.10.1")
 
     implementation ("com.fasterxml.jackson.core:jackson-databind:2.14.0")
+    // https://mvnrepository.com/artifact/org.mariadb.jdbc/mariadb-java-client
+    implementation("org.mariadb.jdbc:mariadb-java-client:3.4.1")
+// https://mvnrepository.com/artifact/com.zaxxer/HikariCP
+    implementation("com.zaxxer:HikariCP:4.0.3")
 
 }
 
